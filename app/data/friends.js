5b58cf2788738a7a4e3e0d9e96e1e084e8855130
Melanie MacDonald <melmacd94@gmail.com>
2017-11-19 00:36:47 -0500

    setup

---
+++ app/data/friends.js
@@ -0,0 +1,62 @@
+var friends = [
+    {
+        name: "Peanut",
+        breed: "Chihuahua",
+        likes: "Tacos, Small Fluffy Beds, Barking",
+        arr: [1,3,4,1,2,4,5,1,5,2]
+    },
+    {
+        name: "Louie",
+        breed: "Cockapoo",
+        likes: "Frisbees, Tummy Rubs, Bacon",
+        arr: [2,3,1,4,5,5,5,2,1,2]
+    },
+    {
+        name: "Dane",
+        breed: "Great Dane",
+        likes: "Sitting on laps, Hugs, Goldfish",
+        arr: [4,5,1,2,2,3,1,4,5,3]
+    },
+      {
+        name: "Emma",
+        breed: "Lab",
+        likes: "People Watching, Eggs, Tug-of-War",
+        arr: [3,4,1,2,4,5,4,4,2,1]
+    },
+      {
+        name: "Bay",
+        breed: "Border Collie",
+        likes: "Small Dark Spaces, Popcorn, Carrying toys",
+        arr: [1,2,5,1,2,4,5,2,1,1]
+    },
+     {
+        name: "Joy",
+        breed: "Golden Retriever",
+        likes: "Meeting People, Cheetos, Fetch",
+        arr: [3,4,5,2,2,1,4,2,4,4]
+    },
+     {
+        name: "Baker",
+        breed: "Dalmation",
+        likes: "Pizza, Dog Park, Snuggles",
+        arr: [4,5,2,5,1,1,2,3,3,3]
+    },
+     {
+        name: "Gracie",
+        breed: "Bischon Frise",
+        likes: "Long walks on the water, Sandwiches, Running in Circles",
+        arr: [5,2,1,4,2,3,1,1,5,5]
+    },
+     {
+        name: "Baloo",
+        breed: "Great Pyrenees",
+        likes: "Peanuts, Doggie Dates, Chasing things",
+        arr: [1,1,1,1,5,2,2,4,1,4]
+    },
+     {
+        name: "Carmel",
+        breed: "Pitbull",
+        likes: "Standing under tables, Running, Biscuits",
+        arr: [5,3,4,2,2,5,1,2,3,3]
+    },
+]
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,7 @@
+app.get("/api/friends", function(req,res){
+//    res.json(friends);
+//   This will be used to display a JSON of all possible friends.
+});
+app.post("/api/friends", function(req, res){
+//This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.    
+});
