50d23b17b951b47741462bae9a56ed47a81f66cc
Melanie MacDonald <melmacd94@gmail.com>
2017-11-20 01:21:40 -0500

    server and math

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,62 +1,39 @@
-var friends = [
+var friendsArray = [
     {
-        name: "Peanut",
-        breed: "Chihuahua",
-        likes: "Tacos, Small Fluffy Beds, Barking",
-        arr: [1,3,4,1,2,4,5,1,5,2]
-    },
-    {
-        name: "Louie",
-        breed: "Cockapoo",
-        likes: "Frisbees, Tummy Rubs, Bacon",
-        arr: [2,3,1,4,5,5,5,2,1,2]
-    },
-    {
-        name: "Dane",
-        breed: "Great Dane",
-        likes: "Sitting on laps, Hugs, Goldfish",
-        arr: [4,5,1,2,2,3,1,4,5,3]
-    },
-      {
-        name: "Emma",
-        breed: "Lab",
-        likes: "People Watching, Eggs, Tug-of-War",
-        arr: [3,4,1,2,4,5,4,4,2,1]
-    },
-      {
-        name: "Bay",
-        breed: "Border Collie",
-        likes: "Small Dark Spaces, Popcorn, Carrying toys",
-        arr: [1,2,5,1,2,4,5,2,1,1]
-    },
-     {
-        name: "Joy",
-        breed: "Golden Retriever",
-        likes: "Meeting People, Cheetos, Fetch",
-        arr: [3,4,5,2,2,1,4,2,4,4]
-    },
-     {
-        name: "Baker",
-        breed: "Dalmation",
-        likes: "Pizza, Dog Park, Snuggles",
-        arr: [4,5,2,5,1,1,2,3,3,3]
-    },
-     {
-        name: "Gracie",
-        breed: "Bischon Frise",
-        likes: "Long walks on the water, Sandwiches, Running in Circles",
-        arr: [5,2,1,4,2,3,1,1,5,5]
-    },
-     {
-        name: "Baloo",
-        breed: "Great Pyrenees",
-        likes: "Peanuts, Doggie Dates, Chasing things",
-        arr: [1,1,1,1,5,2,2,4,1,4]
-    },
-     {
-        name: "Carmel",
-        breed: "Pitbull",
-        likes: "Standing under tables, Running, Biscuits",
-        arr: [5,3,4,2,2,5,1,2,3,3]
-    },
-]
+  name:"Peanut",
+  photo:"https://i.pinimg.com/736x/1f/63/9e/1f639e22e4f076a8f510dac4065f1322--chihuahua-puppies-chihuahuas.jpg",
+  scores:[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+        {
+  name:"Louie",
+  photo:"https://i.pinimg.com/736x/50/71/b2/5071b213c9eb3bbc6ceea6bae42330e1--starbucks-cup-occupation.jpg",
+  scores:[
+      2,
+      3,
+      1,
+      4,
+      5,
+      5,
+      2,
+      2,
+      1,
+      2
+    ]
+},
+    
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,42 @@
+var friends=require("../data/friends")
+var path = require('path');
+
+module.exports = function(app) {
+
 app.get("/api/friends", function(req,res){
-//    res.json(friends);
+    res.json(friendsArray);
 //   This will be used to display a JSON of all possible friends.
 });
 app.post("/api/friends", function(req, res){
-//This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.    
+//This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.        
+        
+var newFriendScores = req.body.scores;
+var scoresArray= [];
+var bestMatch = 0;
+
+for(var i=0; i<friendsArray.length; i++){            
+ var scoresDiff = 0;
+        
+      for(j=0; j< newFriend.scores.length; j++){
+           scoresDiff += Math.abs(newFriendScores.scores[j]- friendsArray[i].scores[j]);
+          
+      }
+      console.log(scoresDiff);
+     
+      scoresArray.push(scoresDiff);
+};
+
+for(var i=0; i<scoresArray.length; i++){
+      if(scoresArray[i] <= scoresArray[bestMatch]){
+        bestMatch = i;
+      }
+}
+
+    var bff = friendsArray[bestMatch];
+    res.json(bff);
+
+
+    friendsArray.push(req.body);
 });
+    
+}
