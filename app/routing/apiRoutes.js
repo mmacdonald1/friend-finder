9606f4df4387e12250648724f280d981a3f3073b
Melanie MacDonald <melmacd94@gmail.com>
2018-01-06 15:38:23 -0500

    git fix

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,7 +11,8 @@ app.post("/api/friends", function(req, res){
 //This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.        
         
 var newFriendScores = req.body;
-    console.log(req.body);
+var scores = newFriendScores.scores;
+    console.log(scores);
 var scoresArray= [];
 var bestMatch = 0;
 
